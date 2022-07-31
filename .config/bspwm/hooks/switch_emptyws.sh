7e00ea6c5e2d881615525aea7d91a6f989470859
xd003 <singhbadal04@gmail.com>
2022-08-01 01:28:53 +0530

    Update switch_emptyws.sh

--- .config/bspwm/hooks/switch_emptyws.sh
+++ .config/bspwm/hooks/switch_emptyws.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-bspc subscribe node_remove | while IFS=' ' read -r e m d n r; do bspc desktop "$d"'.focused.!occupied' -f last; done
+bspc subscribe node_remove | while IFS=' ' read -r e m d n r; do bspc desktop "$d"'.focused.!occupied' -f  'last.occupied'; done
+#bspc subscribe node_remove | while IFS=' ' read -r e m d n r; do for sel in last.occupied prev.occupied '^1'; do bspc desktop "$d"'.focused.!occupied' -f "$sel" && continue; done; done

8638a8d2b05faf1dd0ab83496504cfafc0c1d1a0
xd003 <singhbadal04@gmail.com>
2022-09-02 22:43:56 +0530

    Update autostart.sh

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -9,7 +9,7 @@
 
 # Restart default apps
 declare -a restart=(xfce4-power-manager sxhkd xbanish
-	copyq playerctld picom flameshot touchegg dunst)
+	parcellite playerctld picom flameshot touchegg dunst)
 for i in "${restart[@]}"; do
 	pgrep -x "$i" | xargs kill
   sleep 0.5

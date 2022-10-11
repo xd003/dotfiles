298d0c7e3fdf999bfe8575f1e3a91b7c8d039f91
xd003 <singhbadal04@gmail.com>
2022-10-11 21:48:54 +0530

    Remove stuffs not needed in hyprland

--- .config/bspwm/scripts/switch_emptyws.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#bspc subscribe node_remove | while IFS=' ' read -r e m d n r; do bspc desktop "$d"'.focused.!occupied' -f  'last.occupied'; done
-#bspc subscribe node_remove | while IFS=' ' read -r e m d n r; do for sel in last.occupied prev.occupied '^1'; do bspc desktop "$d"'.focused.!occupied' -f "$sel" && continue; done; done
-bspc subscribe node_remove | while IFS=' ' read -r e m d n r; do bspc query -D -d "$d".focused > /dev/null && while bspc desktop 'focused.!occupied' -r; do :; done done
